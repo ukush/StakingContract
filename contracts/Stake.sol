de5065766c6d1fc45438f724880f02134b4ae266
ukush <ukushi01@gmail.comm>
2024-03-25 11:53:00 +0000

    added basic functionality

--- contracts/Stake.sol
+++ contracts/Stake.sol
@@ -4,5 +4,64 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DEFI is ERC20 {
-    constructor() ERC20("DEFI", "DEFI") {}
+    
+    mapping(address => uint256) stakedAmount;
+    mapping(address => uint256) stakedTimestamp;
+    
+    constructor() ERC20("DEFI", "DEFI") {
+        // total supply as 1M DEFI tokens
+        _mint(address(this), 1000000);
+    }
+
+    function stake(uint256 amount) public {
+        // ensure user has enough to stake
+        require(amount > 0, "You cannot stake 0 tokens");
+        require(balanceOf(msg.sender) >= amount, "You do not have enough tokens");
+
+        // update mapping
+        stakedAmount[msg.sender] += amount;
+
+        // only record the timestamp when amount exceeds 1000 which is when the rewards should 
+        // start being calculated from
+        if (stakedTimestamp[msg.sender] == 0 && stakedAmount >= 1000) {
+            stakedTimestamp[msg.sender] = block.timestamp;
+        }
+
+       // transfer to the contracts address
+        _transfer(msg.sender, address(this), amount);
+
+    }
+
+    function claim() public {
+        require(stakedAmount[msg.sender] > 0, "You have no tokens staked");
+
+        uint256 totalStaked = stakedAmount[msg.sender];
+
+        uint256 rewardPerThousandTokens = 1 * (totalStaked / 1000);
+        // get reward into days
+        uint256 daysElapsed = (block.timestamp - stakedTimestamp[msg.sender]) / 864000;
+        uint256 reward = rewardPerThousandTokens * daysElapsed;
+        
+        uint256 rewardPlusOriginal = reward + stakedAmount[msg.sender];
+
+        // transfer this amount from the supply to the staker
+        _transfer(address(this), msg.sender, rewardPlusOriginal);
+
+        // update the mappings
+        totalStaked[msg.sender] = 0;
+        stakedTimestamp[msg.sender] = 0;
+    }
+
+    function viewRewards() public view returns(uint256) {
+        
+        uint256 totalStaked = stakedAmount[msg.sender];
+
+        uint256 rewardPerThousandTokens = 1 * (totalStaked / 1000);
+        // get reward into days
+        uint256 daysElapsed = (block.timestamp - stakedTimestamp[msg.sender]) / 864000;
+
+        return rewardPerThousandTokens * daysElapsed;
+    }
+
+
 }
